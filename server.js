554d92946c76ff7a893891219bb9a9d13587561e
Angely Dayn Muñoz <angiemunoz960@gmail.com>
2026-05-21 00:20:29 -0500

    Update server configuration

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const ahorrosRoutes = require('./routes/ahorros.routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
